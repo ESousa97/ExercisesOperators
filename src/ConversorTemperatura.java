26de8af5a10e908d37b36444216eb4b6615ac659
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2025-06-03 22:14:11 -0300

    att

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ConversorTemperatura {
+    public static void main(String[] args) {
+        double temperatura1 = 39.7;
+        double temperatura2 = (temperatura1 * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é exatamente: %f Fahrenheit", temperatura1, temperatura2);
+
+        System.out.println(mensagem);
+
+        int temperatura2Inteira = (int) temperatura2;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperatura2Inteira);
+
+    }
 }
